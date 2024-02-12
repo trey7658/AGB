8a9dae5e76bd02d0289e6cffc1fb4cbf78450207
trwy7 <47907719+trwy7@users.noreply.github.com>
2024-02-12 09:57:08 -0700

    back to docker run

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-# docker run -tid -v $(pwd)/data:/data --hostname agbdocker --name agb --restart unless-stopped trwy7/agb
-docker compose up -d
+docker run -tid -v $(pwd)/data:/data --hostname agbdocker --name agb --restart unless-stopped trwy7/agb
