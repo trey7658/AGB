6042673bc7447e7f59f2bbab36060d7b60a391c7
trwy7 <47907719+trwy7@users.noreply.github.com>
2024-02-12 08:13:24 -0700

    Move start.sh to docker compose

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-docker run -tid -v /media/Bot/discordbot/data:/data --hostname agbdocker --name agb --restart unless-stopped trwy7/agb
+# docker run -tid -v /media/Bot/discordbot/data:/data --hostname agbdocker --name agb --restart unless-stopped trwy7/agb
+docker compose up -d
