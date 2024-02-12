e95fe2d054eed4d5305914296a7a487b8df9bcc0
trwy7 <47907719+trwy7@users.noreply.github.com>
2024-02-11 19:21:26 -0700

    Check for dockerfile

--- update.sh
+++ update.sh
@@ -4,6 +4,12 @@ if [ "$EUID" -ne 0 ]
 	sudo ./update.sh
 	exit
 fi
+if [ -f Dockerfile ]
+	then echo "Dockerfile exists, continuing..."
+else
+	echo "Dockerfile does not exist, please run this script in the same directory as the Dockerfile, or make sure the Dockerfile exists. You can use the example in the repository."
+	exit
+fi
 git stage *
 echo Make sure you have ran /backup and /shutdown if the bot is running, you have 5 seconds to CTRL-C
 sleep 5s
