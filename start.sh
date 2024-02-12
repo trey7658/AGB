54191d35e67a2f1d6efab3876ec384a55ea5a0d1
trwy7 <47907719+trwy7@users.noreply.github.com>
2024-02-12 08:17:02 -0700

    Remove hard coded directory

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-# docker run -tid -v /media/Bot/discordbot/data:/data --hostname agbdocker --name agb --restart unless-stopped trwy7/agb
+# docker run -tid -v $(pwd)/data:/data --hostname agbdocker --name agb --restart unless-stopped trwy7/agb
 docker compose up -d
