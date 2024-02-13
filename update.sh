72b65ebf8eb14d6dc5329c28623dfec6613ba265
Trey <47907719+trwy7@users.noreply.github.com>
2024-02-13 07:10:46 -0700

    Remove git from update.sh

--- update.sh
+++ update.sh
@@ -10,11 +10,8 @@ else
 	echo "Dockerfile does not exist, please run this script in the same directory as the Dockerfile, or make sure the Dockerfile exists. You can use the example in the repository."
 	exit
 fi
-git stage *
 echo Make sure you have ran /backup and /shutdown if the bot is running, you have 5 seconds to CTRL-C
 sleep 5s
-read -p "Commit message: " commit
-git commit -m "$commit"
 sudo docker build -t trwy7/agb .
 sudo docker stop agb
 sudo docker remove agb
